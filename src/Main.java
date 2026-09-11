4c25771f951451fb9dc0c343b8df1d047f5a7265
Федорков Александр <fedorkov@cifra-bank.ru>
2026-09-11 03:02:20 +0300

    commit for first branch

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
   public static void main(String[] args) {
+    System.out.println(Arrays.toString(args));
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     System.out.printf("Hello and welcome!");
